dbd12e0b388031ab26925cb2889d1c367088cf83
Andrew Xia <andrewbenxia@gmail.com>
2025-06-14 03:19:58 -0500

    port

--- scripts/helper.js
+++ scripts/helper.js
@@ -1,4 +1,5 @@
-const baseurl = window.location.host; // give them the port if they wanna!!!!
+const baseurl = window.location.origin; // give them the port if they wanna!!!!
+const fullurl = baseurl + window.location.pathname;
 const debug = baseurl !== "andrewb.xyz";
 // helper code i wrote to make everything(javascript) either less horrible or more unreadable with the terseness of whatever declaratinons come out of me utilizing this
 
