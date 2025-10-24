53d3a4a2fb96266fd93d11706fa7652265912940
andrewxia <andrewbenxia@gmail.com>
2025-10-24 13:40:00 -0500

    m

--- work/work.js
+++ work/work.js
@@ -9,7 +9,7 @@ const size = pint(elprop(cube, "--size"));
 attachdebug(size, "size");
 
 const returncubetransform = (props = {}) => {
-    const rotatex = props.rotatex || 0;
+    const rotatex = props.rotatex || 90;
     const rotatey = props.rotatey || 0;
     const rotatez = props.rotatez || 0;
     const perspective = props.perspective || size / 2;
@@ -22,9 +22,19 @@ const returncubetransform = (props = {}) => {
 document.addEventListener("mousemove", (e) => {
     const x = e.clientX / window.innerWidth;
     const y = e.clientY / window.innerHeight;
+    const midheight = window.innerHeight / 2;
+    const midwidth = window.innerWidth / 2;
+    const xdiff = e.clientX - midwidth;
+    const ydiff = e.clientY - midheight;
+    const rotateX = (ydiff / midheight) * 90;
+    const rotateY = (xdiff / midwidth) * 90;
 
     cube.style.transform = returncubetransform({
         translatex: x * size,
-        translatey: y * size
+        translatey: y * size,
+        // rotatex: rotateX,
+        // rotatey: rotateY
     });
 });
+
+document.dispatchEvent(new MouseEvent("mousemove"));
